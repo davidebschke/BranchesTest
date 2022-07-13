1071adcffa5f26bfe236ee5974d9a18575c24722
davidebschke <Davidebschke8@gmail.com>
2022-07-13 16:01:58 +0200

    Bug by SayWelcome fix

--- src/main/java/com/example/branchestest/Controller.java
+++ src/main/java/com/example/branchestest/Controller.java
@@ -1,6 +1,7 @@
 package com.example.branchestest;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
 
+    @GetMapping
     public String sayWelcome(){
 
 
