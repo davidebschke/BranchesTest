c2c27ab82c2c62ef7a35b13066f1d2e3deb9cf33
davidebschke <Davidebschke8@gmail.com>
2022-07-13 16:00:01 +0200

    Initialize

---
+++ src/main/java/com/example/branchestest/Controller.java
@@ -0,0 +1,18 @@
+package com.example.branchestest;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+
+
+
+
+
+
+
+}
